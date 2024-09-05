2be5a71b32e67433861f698222984211d407e76b
IvleevaViktoria <118309123+IvleevaViktoria@users.noreply.github.com>
2024-09-06 01:36:50 +0300

    main .cpp

--- main (4).cpp
+++ main (4).cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <fstream>
-#include <vector>
-#include <iomanip>
+#include <string.h>
+#include <stdlib.h>
 
 #define MAX_MANAGERS 100
 #define MAX_NAME_LENGTH 100
@@ -15,12 +14,12 @@ struct Manager {
 
 struct Trade {
     Manager list[MAX_MANAGERS];
-    int count = 0; // Начинаем с нуля
+    int count; // Начинаем с нуля
 };
 
-void add_manager(Trade *trade, const Manager &manager) {
+void add_manager(Trade *trade, const Manager *manager) {
     if (trade->count < MAX_MANAGERS) {
-        trade->list[trade->count++] = manager; // Добавляем менеджера в список
+        trade->list[trade->count++] = *manager; // Добавляем менеджера в список
     }
 }
 
@@ -32,22 +31,23 @@ void read_managers(Trade *trade, const char *filename) {
     }
 
     char line[256];
+    trade->count = 0; // инициализируем количество менеджеров
     while (fgets(line, sizeof(line), file)) {
         Manager m;
         char salary[20];
 
         sscanf(line, "%d;%[^;];%[^;];%s", &m.id, m.name, m.city, salary);
         m.salary = atof(salary);
-        add_manager(trade, m);
+        add_manager(trade, &m);
     }
     fclose(file);
 }
 
-void print_kovrov_managers(const Trade &trade) {
+void print_kovrov_managers(const Trade *trade) {
     printf("Ковровские менеджеры:\n");
-    for (int i = 0; i < trade.count; i++) {
-        if (strcmp(trade.list[i].city, "КОВРОВ") == 0) {
-            printf("%d %s %s %.2f", trade.list[i].id, trade.list[i].name, trade.list[i].city, trade.list[i].salary);
+    for (int i = 0; i < trade->count; i++) {
+        if (strcmp(trade->list[i].city, "КОВРОВ") == 0) {
+            printf("%d %s %s %.2f", trade->list[i].id, trade->list[i].name, trade->list[i].city, trade->list[i].salary);
         }
     }
 }
@@ -55,6 +55,6 @@ void print_kovrov_managers(const Trade &trade) {
 int main() {
     Trade trade;
     read_managers(&trade, "manager.txt");
-    print_kovrov_managers(trade);
+    print_kovrov_managers(&trade);
     return 0; // Завершение программы
 }
